ee834b61a6acbbccdc51f556a94b0c4ac4abae37
ahmadrgul <ahmadrg91@gmail.com>
2025-08-04 17:14:39 +0500

    feat(backend/users): add custom-user manager and add user role

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
+    """
+    Custom user model that will store only the required fields,
+    it will be extended using profile models for differet user types.
+    """
     first_name = None
     last_name = None
     username = None
     email = models.EmailField(unique=True)
+    USER_ROLE_CHOICES = {
+        "admin": "Admin",
+        "organizer": "Organizer",
+        # can be extended with more roles
+    }
+    role = models.CharField(default='organizer', choices=USER_ROLE_CHOICES)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
