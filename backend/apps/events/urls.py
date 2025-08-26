594f96d6c7502b6d016726f8afdbc521750853ae
ahmadrgul <ahmadrg91@gmail.com>
2025-08-26 16:38:48 +0500

    fix(backend/events): add tickets urls

--- backend/apps/events/urls.py
+++ backend/apps/events/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet
+from .views import EventViewSet, TicketViewSet
 
 router = DefaultRouter()
 router.register(r'events', EventViewSet, basename='event')
+router.register(r'tickets', TicketViewSet, basename='ticket')
 
 urlpatterns = router.urls
