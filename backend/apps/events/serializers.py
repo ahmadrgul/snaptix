7b9249af2684267434eca052f2deb52026ac432a
ahmadrgul <ahmadrg91@gmail.com>
2025-08-26 15:32:12 +0500

    feat(backend/events): add serializer for event and ticket model

---
+++ backend/apps/events/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Event, Ticket
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+        read_only_fields = ('created_at', 'updated_at', 'organizer')
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = '__all_'
+        read_only_fields = ('created_at', 'updated_at')
