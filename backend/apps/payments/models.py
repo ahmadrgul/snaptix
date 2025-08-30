d3f54b93256b83fa232954b12741d3fbb837e6af
ahmadrgul <ahmadrg91@gmail.com>
2025-08-30 15:43:54 +0500

    fix(backend/payments): change id of Payment model to be UUID instead of INT

--- backend/apps/payments/models.py
+++ backend/apps/payments/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 from djmoney.models.fields import MoneyField
 
 class Payment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     stripe_payment_intent_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
     ticket = models.OneToOneField('events.Ticket', on_delete=models.CASCADE, related_name='payment')
     amount = MoneyField(max_digits=10, decimal_places=2, default_currency='USD', default=0)
